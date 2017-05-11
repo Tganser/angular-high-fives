8df52c84181397e33fbe5892eb13f9f2d300742f
Tessa Ganser <t.m.ganser@gmail.com>
2017-05-11 14:24:44 -0500

    random number generator working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,13 +5,29 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('TheController', function(HighFive){
   var vm = this;
-  var ranNum = 0;
+  var characterSkillLevel;
+  var successNum;
 
   vm.highFive = function() {
     console.log("in highFive button!!");
-    console.log(ranNum);
-    ranNum = HighFive.generateNum(1,10);
-    console.log(HighFive.generateNum(1,10));
-    console.log(ranNum);
+    console.log("ranNum to start : ",ranNum);
+    characterSkillLevel = HighFive.generateNum(1,10);
+    successNum = HighFive.generateNum(1,10);
+    console.log("after running random number funciton: ",HighFive.generateNum(1,10));
+    // console.log("ran num after: ", ranNum);
+
+
+    if (successNum >= characterSkillLevel){
+      console.log("GREAT HIGH FIVE TEAM");
+      HighFive.updateCounter();
+      console.log("This is the count: "+HighFive.getCount);
+    }
+    else {
+      console.log("try again with the fives");
+    }
   };
+
+
+
+
 });
--- server/public/scripts/high-five-service.js
+++ server/public/scripts/high-five-service.js
@@ -1,15 +1,26 @@
 myApp.service('HighFive', function(){
 
   var theNum;
+  var successCounter = 0;
 
   this.generateNum = function(min, max){
-      console.log("in generateNum");
+      // console.log("in generateNum");
+      // console.log(Math.floor(Math.random() * (1 + max - min) + min));
       theNum = Math.floor(Math.random() * (1 + max - min) + min);
+      return theNum;
     };
 
-  this.numGetter = function(){
-    console.log("in numGetter");
-    return theNum;
+
+
+  this.updateCounter = function(){
+    console.log("updating the counter!");
+    successCounter++;
+    return successCounter;
+  };
+
+  this.getCount = function(){
+    console.log("in getCount");
+    return successCounter;
   };
 
 });
